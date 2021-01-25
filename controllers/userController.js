164cb13f9465e4cf00b1adc3b1ed6fd41a94ead1
rojiwon0325 <bond9986@naver.com>
2021-01-25 22:09:41 +0900

    Initial Commit

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("login");
+export const logout = (req, res) => res.send("logout");
+
+
+export const users = (req, res) => res.send("User");
+export const userDetail = (req, res) => res.send("UserDetail");
+export const editProfile = (req, res) => res.send("EditProfile");
+export const changePassword = (req, res) => res.send("ChangePassword");
--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 4000;
-
-function handleListening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-app.listen(PORT, handleListening);
